5f7c34a42086a46a94799161199441955f98d382
1102137116 <1102137116@gm.kuas.edu.tw>
2016-03-17 16:21:15 +0800

    0317

---
+++ Sale/Controllers/OrderController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Sale.Controllers
+{
+    public class OrderController : Controller
+    {
+        /// <summary>
+        /// 訂單管理系統首頁
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Index()
+        {
+            Models.OrderService orderService = new Models.OrderService();
+            var order = orderService.GetOrderById("111");
+            ViewBag.CustId = order.CustId;
+            ViewBag.CustName = order.CustName;
+            return View();
+        }
+    }
+}
